5197a914764b7791a49984db1ad2fd947a07caa4
unknown <xlao@IDS-DESIGN-2189.menards.net>
2016-06-18 07:55:42 -0500

    destroying

---
+++ UnityProj/Assets/Main/Scripts/CollisionDestroyerByInterface.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDestroyerByInterface : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.GetComponent<MonoBehaviour>() is IKillable) {
+            Destroy(col.gameObject);
+        }
+    }
+	
+}
---
+++ UnityProj/Assets/Main/Scripts/Lao/Generics/destroyObject/DestroyMeBelowGround.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LAO.Generic {
+
+    public class DestroyMeBelowGround : MonoBehaviour {
+        public float yAxis = -10f;
+        // Update is called once per frame
+        void Update() {
+            if (this.gameObject.transform.position.y <= yAxis) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
